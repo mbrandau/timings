9f21b4d1d8a9c8c27b7826d17edbf34c7f44d301
Maximilian Brandau <mbrandau@online.de>
2017-09-12 17:38:43 +0200

    Add possibility to save timings

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-module.exports = init;
+module.exports = createTracker;
+module.exports.getTimings = getTimings;
+module.exports.getAverage = getAverage;
 
-function init() {
+const timings = {};
+
+function createTracker(trackerName) {
   let startTime = process.hrtime();
   let lastTime = undefined;
-  return function() {
+  return function(itemName) {
     const diff = process.hrtime(lastTime || startTime);
     lastTime = process.hrtime();
-    return diff[0] * 1e3 + diff[1] / 1e6;
+    const diffMillis = diff[0] * 1e3 + diff[1] / 1e6;
+
+    if (trackerName && itemName) {
+      if (timings[trackerName]) {
+        if (!timings[trackerName][itemName]) timings[trackerName][itemName] = [diffMillis];
+        else timings[trackerName][itemName].push(diffMillis);
+      } else {
+        timings[trackerName] = {};
+        timings[trackerName][itemName] = [diffMillis];
+      }
+    }
+
+    return diffMillis;
+  };
+}
+
+function getTimings(tracker, item) {
+  if (tracker) {
+    if (item) return timings[tracker][item];
+    return timings[tracker];
+  }
+  return timings;
+}
+
+function getAverage(tracker, item) {
+  if (!timings[tracker] || !timings[tracker][item]) return null;
+  let avg = 0;
+  const array = timings[tracker][item];
+  for (var i = 0; i < array.length; i++) avg += array[i];
+  return {
+    times: array.length,
+    average: avg / array.length
   };
 }
--- test/index.js
+++ test/index.js
@@ -1,27 +1,69 @@
-const expect = require('expect.js');
+const expect = require('expect.js'),
+  timings = require('../index');
 
 describe('timings', function() {
   it('should expose a function', function() {
-    expect(require('../index')).to.be.a('function');
+    expect(timings).to.be.a('function');
   });
   it('should expose a tracker function on call', function() {
-    expect(require('../index')()).to.be.a('function');
+    expect(timings()).to.be.a('function');
   });
   describe('tracker', function() {
     it('should return a number', function() {
-      expect(require('../index')()()).to.be.a('number');
+      expect(timings()()).to.be.a('number');
     });
     it('should be below 1ms', function() {
       const tracker = require('../index')();
       expect(tracker()).to.be.below(1);
     });
-    it('should be between 19ms and 21ms', function(done){
-      const tracker = require('../index')();
+    it('should be between 19ms and 21ms', function(done) {
+      const tracker = timings();
       tracker();
-      setTimeout(function(){
+      setTimeout(function() {
         expect(tracker()).to.be.greaterThan(19).and.lessThan(21);
         done();
       }, 20);
+    });
+  });
+  describe('getTimings', function() {
+    it('should return an array when called with both tracker- and item name', function() {
+      const tracker = timings('test1');
+      tracker();
+      tracker('time');
+      tracker('time');
+      const array = timings.getTimings('test1', 'time');
+      expect(array).to.be.an('array');
+      expect(array).to.have.length(2);
+    });
+    it('should return an object with item names when called with tracker name', function() {
+      const tracker = timings('test2');
+      tracker('t1');
+      tracker('t1');
+      tracker('t2');
+      const obj = timings.getTimings('test2');
+      expect(obj).to.only.have.keys('t1', 't2');
+      expect(obj.t1).to.be.an('array');
+      expect(obj.t1).to.have.length(2);
+      expect(obj.t2).to.be.an('array');
+      expect(obj.t2).to.have.length(1);
+    });
+    it('should return an object with all timings when called without arguments', function(){
+      const obj = timings.getTimings();
+      expect(obj).to.only.have.keys('test1','test2');
+    });
+  });
+  describe('getAverage', function() {
+    it('should return an object with keys times and average', function() {
+      const result = timings.getAverage('test1', 'time');
+      expect(result).to.only.have.keys('times', 'average');
+      expect(result.average).to.be.a('number');
+    });
+    it('result should have proper times value', function(){
+      expect(timings.getAverage('test1','time').times).to.be(2);
+    });
+    it('should return null if no items were found', function() {
+      expect(timings.getAverage('not', 'existent')).to.be(null);
+      expect(timings.getAverage('test1', 'empty')).to.be(null);
     });
   });
 });
